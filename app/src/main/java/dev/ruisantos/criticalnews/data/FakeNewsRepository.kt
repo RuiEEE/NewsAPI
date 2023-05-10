19c9af19b761c2d7a10d5bb5f5fc8222c83a4fdf
Rui Santos <ruiapdsantos@gmail.com>
2023-05-10 23:20:00 +0100

    test main view model

---
+++ app/src/main/java/dev/ruisantos/criticalnews/data/FakeNewsRepository.kt
@@ -0,0 +1,30 @@
+package dev.ruisantos.criticalnews.data
+
+import dev.ruisantos.criticalnews.network.Article
+import dev.ruisantos.criticalnews.network.NewsSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
+
+class FakeNewsRepository : NewsRepository {
+
+    val articles = mutableListOf(
+        Article(
+            source = NewsSource(
+                id = "cnn",
+                name = "CNN"
+            ),
+            title = "Lorem Ipsum",
+            description = "Big Lorem Ipsum",
+            content = "Big Lorem Ipsum, but limited to 200 characters",
+            url = "cnn.com",
+            publishedAt = "now",
+            urlToImage = "https://picsum.photos/200/300"
+        )
+    )
+
+    override fun getNews(): Flow<List<Article>> = flow {
+        emit(articles)
+    }
+}
---
+++ app/src/test/java/dev/ruisantos/criticalnews/MainViewModelTest.kt
@@ -0,0 +1,38 @@
+package dev.ruisantos.criticalnews
+
+import dev.ruisantos.criticalnews.data.FakeNewsRepository
+import dev.ruisantos.criticalnews.ui.main.MainViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class MainViewModelTest {
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private lateinit var viewModel: MainViewModel
+    private val fakeNewsRepository = FakeNewsRepository()
+
+    @Before
+    fun setup() {
+        viewModel = MainViewModel(fakeNewsRepository)
+    }
+
+    @Test
+    fun `Headline UI is shown after fetching the Headlines` () = runTest {
+        viewModel.fetchHeadlines()
+        assert(viewModel.ui.value is MainViewModel.UI.Headlines)
+        assert((viewModel.ui.value as MainViewModel.UI.Headlines).articles.size == fakeNewsRepository.articles.size)
+    }
+
+    @Test
+    fun `Initial state is headlines`() = runTest {
+        assert(viewModel.ui.value is MainViewModel.UI.Headlines)
+    }
+}
