25185efddd154c6e8a5179eb9ceed793467fb221
Rui Santos <ruiapdsantos@gmail.com>
2023-05-10 21:28:58 +0100

    add biometric capabilities

--- app/src/main/java/dev/ruisantos/criticalnews/ui/main/MainFragment.kt
+++ app/src/main/java/dev/ruisantos/criticalnews/ui/main/MainFragment.kt
@@ -1,11 +1,16 @@
 package dev.ruisantos.criticalnews.ui.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -16,8 +21,18 @@ import dev.ruisantos.criticalnews.data.NewsRepositoryImpl
 import dev.ruisantos.criticalnews.databinding.FragmentMainBinding
 import dev.ruisantos.criticalnews.network.RetrofitCriticalNews
 import kotlinx.coroutines.launch
+import java.util.concurrent.Executor
 
 class MainFragment : Fragment() {
+
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
+    val launchSettings =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            // Got back from the settings
+        }
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
@@ -44,8 +59,16 @@ class MainFragment : Fragment() {
         binding.apply {
             rvHeadlines.apply {
                 adapter = HeadlineAdapter(mutableListOf()) {
-                    findNavController().navigate(MainFragmentDirections.actionMainFragmentToDetailFragment(it))
+                    findNavController().navigate(
+                        MainFragmentDirections.actionMainFragmentToDetailFragment(
+                            it
+                        )
+                    )
                 }
+            }
+
+            bAuth.setOnClickListener {
+                showBiometricPrompt()
             }
         }
 
@@ -53,14 +76,15 @@ class MainFragment : Fragment() {
             viewModel.ui.collect { ui ->
                 when (ui) {
                     is MainViewModel.UI.Headlines -> setupHeadlines(ui)
+                    MainViewModel.UI.Authentication -> showBiometricPrompt()
+                    MainViewModel.UI.Settings -> openSettings()
                 }
             }
         }
-    }
 
-    override fun onResume() {
-        super.onResume()
-        viewModel.fetchHeadlines()
+        lifecycleScope.launch {
+            viewModel.checkBiometricAvailability(requireContext())
+        }
     }
 
     private fun setupHeadlines(headlines: MainViewModel.UI.Headlines) {
@@ -72,6 +96,57 @@ class MainFragment : Fragment() {
                 }
             }
         }
+    }
+
+    fun getHeadlines() {
+        viewModel.fetchHeadlines()
+    }
+
+    private fun openSettings() {
+        Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+            putExtra(
+                Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL
+            )
+        }.also {
+            launchSettings.launch(it)
+        }
+    }
+
+    private fun showBiometricPrompt() {
+        // Show fingerprint prompt
+        executor = ContextCompat.getMainExecutor(requireContext())
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(
+                    errorCode: Int,
+                    errString: CharSequence
+                ) {
+                    super.onAuthenticationError(errorCode, errString)
+                    binding.bAuth.visibility = View.VISIBLE
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult
+                ) {
+                    super.onAuthenticationSucceeded(result)
+                    binding.bAuth.visibility = View.GONE
+                    getHeadlines()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    binding.bAuth.visibility = View.VISIBLE
+                }
+            })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Authenticate to continue")
+            .setSubtitle("Get in your latest news!")
+            .setNegativeButtonText("Cancel")
+            .build()
+
+        biometricPrompt.authenticate(promptInfo)
     }
 
 }
--- app/src/main/java/dev/ruisantos/criticalnews/ui/main/MainViewModel.kt
+++ app/src/main/java/dev/ruisantos/criticalnews/ui/main/MainViewModel.kt
@@ -1,5 +1,9 @@
 package dev.ruisantos.criticalnews.ui.main
 
+import android.content.Context
+import android.content.Intent
+import android.provider.Settings
+import androidx.biometric.BiometricManager
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dev.ruisantos.criticalnews.data.NewsRepository
@@ -11,10 +15,13 @@ import kotlinx.coroutines.launch
 
 class MainViewModel(val repository: NewsRepository) : ViewModel() {
 
+    var hasAuthenticated = false
+
     private val _ui = MutableStateFlow<UI>(UI.Headlines(mutableListOf()))
     val ui: StateFlow<UI> = _ui
 
     fun fetchHeadlines() {
+        hasAuthenticated = true
         viewModelScope.launch {
             repository.getNews().catch {
                 it.printStackTrace()
@@ -24,8 +31,24 @@ class MainViewModel(val repository: NewsRepository) : ViewModel() {
         }
     }
 
+    suspend fun checkBiometricAvailability(context: Context) {
+        if(hasAuthenticated) {
+            return
+        }
+
+        val biometricManager = BiometricManager.from(context)
+        when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL)) {
+            BiometricManager.BIOMETRIC_SUCCESS -> _ui.emit(UI.Authentication)
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> fetchHeadlines()
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> fetchHeadlines()
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> _ui.emit(UI.Settings)
+        }
+    }
+
     sealed class UI {
         class Headlines(val articles: List<Article>) : UI()
+        object Authentication : UI()
+        object Settings : UI()
     }
 
 }
